725d77e9b953a0926425ae879c336e75e67104cd
Nick Collins <nickcollins24@gmail.com>
2018-09-30 02:55:20 -0700

    update salt statements

--- ff_bot/salter.py
+++ ff_bot/salter.py
@@ -10,13 +10,12 @@ def throw_salt(name):
     verb0 = str.lower(VERBS[random.randint(0, len(VERBS)-1)])
 
     salt0 = name + " is a " + adjective0 + " " + noun0 + "."
-    salt1 = name + " likes to " + verb0 + " a " + adjective0 + " " + noun0 + " before breakfast."
-    salt2 = name + " smells like a " + adjective0 + " " + noun0 + ", but worse."
-    salt3 = name + " looks like a " + adjective0 + " " + noun0 + "."
-    salt4 = "i have never seen a " + noun0 + " as " + adjective0 + " as " + name + "."
-    salt5 = name + " is the biggest " + noun0 + " that ever lived!"
-    salt6 = "you can't spell " + name + " without " + noun0 + "."
-    salt7 = "hey " + name + ", go " + verb0 + " a " + noun0 + "!"
-    salts = [salt0, salt1, salt2, salt3, salt4, salt5, salt6,salt7]
+    salt1 = name + " smells like a " + adjective0 + " " + noun0 + "."
+    salt2 = name + " looks like a " + adjective0 + " " + noun0 + "."
+    salt3 = "i have never seen a " + noun0 + " as " + adjective0 + " as " + name + "."
+    salt4 = name + " is the biggest " + noun0 + " that ever lived!"
+    salt5 = "you can't spell " + name + " without " + noun0 + "."
+    salt6 = "hey " + name + ", go " + verb0 + " a " + noun0 + "!"
+    salts = [salt0, salt1, salt2, salt3, salt4, salt5, salt6]
 
     return salts[random.randint(0, len(salts)-1)]
